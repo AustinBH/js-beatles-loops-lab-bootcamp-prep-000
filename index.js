ad04b3f3b756059f5fcb65b0f6d57e6791825fa6
Austin Harlow <harlowaustin2@gmail.com>
2019-04-28 23:11:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return allMusicians;
 }
+
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var fanFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    fanFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return fanFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var love = [];
+  do {
+    love.push("I Love the Beatles!");
+    number++
+  } while (number < 15);
+
+return love;
+}
